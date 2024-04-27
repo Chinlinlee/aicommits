5f5c3132d78f66aa8a4bd0505a50643253337ed4
chinlinlee <a5566qq2581@gmail.com>
2024-04-27 14:18:36 +0800

    feat(conventional): Update commit template, import in prompt generation
    - Update the conventional commit template in a new file
    - Import the conventional commit template to prompt utils
    - Remove the detailed conventional commit template from the prompt utils

---
+++ src/prompt-templates/conventional.ts
@@ -0,0 +1,15 @@
+export const conventionalCommitTemplate = [
+    '#### Part 1: Choose the Best Type\n',
+    'Choose a type from the following options that best describes the git diff:',
+    '- build: Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)',
+    '- chore: Updating libraries, copyrights or other repo setting, includes updating dependencies.',
+    '- ci: Changes to our CI configuration files and scripts (example scopes: Travis, Circle, GitHub Actions)',
+    '- docs: Non-code changes, such as fixing typos or adding new documentation (example scopes: Markdown file)',
+    '- feat: a commit of the type feat introduces a new feature to the codebase',
+    '- fix: A commit of the type fix patches a bug in your codebase',
+    '- perf: A code change that improves performance',
+    '- refactor: A code change that neither fixes a bug nor adds a feature',
+    '- style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
+    '- test: Adding missing tests or correcting existing tests\n',
+    'Provide a summary in the format: `<type>(<optional scope>): <commit message>`'
+].join('\n');
--- src/utils/prompt.ts
+++ src/utils/prompt.ts
@@ -1,5 +1,6 @@
 import type { CommitType } from './config.js';
 import { summarizeFileDiffTemplate } from '../prompt-templates/summarize-file-diff.js';
+import { conventionalCommitTemplate } from '../prompt-templates/conventional.js';
 
 const commitTypeFormats: Record<CommitType, string> = {
 	'': '<commit message>',
@@ -19,24 +20,7 @@ const commitTypes: Record<CommitType, string> = {
 	 * Conventional Changelog:
 	 * https://github.com/conventional-changelog/conventional-changelog/blob/d0e5d5926c8addba74bc962553dd8bcfba90e228/packages/conventional-changelog-conventionalcommits/writer-opts.js#L182-L193
 	 */
-	conventional: `#### Part 1: Choose the Best Type\nChoose a type from the type-to-description JSON below that best describes the git diff, with a maximum of 100 words as title. Add two line breaks at the end and continue with a bullet-based body:\n\n\`\`\`json\n${JSON.stringify(
-		{
-			docs: 'Documentation only changes',
-			style:
-				'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
-			refactor: 'A code change that neither fixes a bug nor adds a feature',
-			perf: 'A code change that improves performance',
-			test: 'Adding missing tests or correcting existing tests',
-			build: 'Changes that affect the build system or external dependencies',
-			ci: 'Changes to our CI configuration files and scripts',
-			chore: "Other changes that don't modify src or test files",
-			revert: 'Reverts a previous commit',
-			feat: 'A new feature',
-			fix: 'A bug fix',
-		},
-		null,
-		2
-	)}\n\`\`\``,
+	conventional: conventionalCommitTemplate,
 };
 
 export const generatePrompt = (
@@ -46,9 +30,9 @@ export const generatePrompt = (
 ) =>
 	[
 		'### Task: Summarize Git Diff with Commit Messages\n',
-		'Whole content should within ' + maxLength + ' characters\n',
+		'- Message language: ' + locale,
+		'- Whole content should within ' + maxLength + ' characters\n',
 		commitTypes[type],
-		specifyCommitFormat(type),
 		'\n#### Part 2: Summarize Git Diff\n',
 		summarizeFileDiffTemplate
 	]
